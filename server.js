6bbfe64dfff29cbc6235e07172012d631c5f594a
cassianocarli <138841588+cassianocarli@users.noreply.github.com>
2024-11-23 16:12:25 -0300

    teste da api hospedada n3

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');  // Para lidar com data e hora
+const cors = require('cors');
+app.use(cors());
 
 const app = express();
 
